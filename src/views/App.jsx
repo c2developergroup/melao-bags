d63b42aede762a574d570ef290d5346332395724
bynx <sebastiancastrorajbe@gmail.com>
2024-01-24 21:29:00 -0400

    contact creado y agregado

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -76,7 +76,69 @@ export const App = () => {
           </button>
         </div>
       </section>
-      <section></section>
+      <section className="p-20 flex">
+        <div className="bg-[url('/contact.png')] w-[50%] bg-cover p-20"></div>
+        <div className="w-[50%] resp:w-full p-20 mobl:p-10 flex justify-center">
+          <div className="w-full">
+            <h2 className="md:text-5xl text-3xl text-black mb-5">
+              Contáctanos
+            </h2>
+            <form className="font-['Smooch_Sans']">
+              <input
+                type="text"
+                name="name"
+                placeholder="Nombres y Apellidos"
+                required
+                maxlength="100"
+                className="appearance-none border-none text-black mr-3 leading-tight focus:ring-0 focus:outline-none w-full"
+              />
+              <div className="border-b border-red-600 py-1 px-2 mb-5"></div>
+              <input
+                type="text"
+                name="email"
+                placeholder="Correo electrónico"
+                required
+                maxlength="100"
+                className="appearance-none border-none text-black mr-3 leading-tight focus:ring-0 focus:outline-none w-full"
+              />
+              <div className="border-b border-red-600 py-1 px-2 mb-5"></div>
+              <input
+                type="text"
+                name="number"
+                placeholder="Número de teléfono"
+                required
+                maxlength="100"
+                className="appearance-none border-none text-black mr-3 leading-tight focus:ring-0 focus:outline-none w-full"
+              />
+              <div className="border-b border-red-600 py-1 px-2 mb-5"></div>
+              <input
+                type="text"
+                name="subject"
+                placeholder="Asunto"
+                required
+                maxlength="100"
+                className="appearance-none border-none text-black mr-3 leading-tight focus:ring-0 focus:outline-none w-full"
+              />
+              <div className="border-b border-red-600 py-1 px-2 mb-8"></div>
+              <div className="mt-5 flex flex-col border rounded-lg border-red-600 p-2 mb-10">
+                <textarea
+                  name="message"
+                  placeholder="Mensaje"
+                  className="appearance-none border-none text-black mr-3 leading-tight focus:ring-0 focus:outline-none resize-none"
+                  required
+                  maxlength="1000"
+                  rows="6"
+                ></textarea>
+                <div className="flex justify-end">
+                  <button className="bg-red-600 rounded-full w-5 h-5 p-[0.2rem] flex justify-center items-center hover:bg-blue-700 cursor-pointer transition duration-300">
+                    <img src="send.svg" class="ml-[0.1rem] w-[80%] h-auto" />
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
